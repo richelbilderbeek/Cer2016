f2e615ade08e3714936b2624dbcdf8ff93984f16
richelbilderbeek <richel@richelbilderbeek.nl>
2016-04-26 14:38:45 +0200

    Made Travis happier

--- R/plot_extant_incipient_tree.R
+++ R/plot_extant_incipient_tree.R
@@ -1,6 +1,13 @@
 #' Plot the extant inicipient species tree
+#' @param filename name of the parameter file
+#' @return Nothing. It does produce a plot
+#' @export
 plot_extant_incipient_tree <- function(filename) {
-  testit::assert(is_valid_file(filename))
+  if (!is_valid_file(filename)) {
+    stop("plot_extant_incipient_tree: ",
+      "file '", filename, "' is invalid"
+    )
+  }
   base_filename <- basename(filename)
   file <- read_file(filename)
   plot(file$pbd_output[[1]], main = paste(
