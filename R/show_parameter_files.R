5215faaf5f834f3edfa35cb7c14a5ec28c6c02bf
richelbilderbeek <richel@richelbilderbeek.nl>
2016-05-03 17:30:49 +0200

    Tranferring worked-out example from LyX to vignette

---
+++ R/show_parameter_files.R
@@ -0,0 +1,37 @@
+#' Creates a nice knitr::table to show one or more parameter files
+#' @param filenames names of the parameter files
+#' @export
+show_parameter_files <- function(filenames) {
+  for (filename in filenames) {
+    if (!file.exists(filename)) {
+      stop(
+        "show_parameter_file: ",
+        "file '", filename, "' not found"
+      )
+    }
+  }
+  n_files <- length(filenames)
+
+  names(read_file(filenames[1])$parameters)
+  df <- data.frame(
+    parameter = names(read_file(filenames[1])$parameters),
+    stringsAsFactors = FALSE
+  )
+
+  # Disable scientific notation
+  old_scipen <- getOption("scipen")
+  options(scipen = 999)
+
+  for (i in 1:n_files) {
+    file <- read_file(filenames[i])
+    df <- cbind(df, as.numeric(read_file(filenames[i])$parameters[2, , 2]))
+  }
+
+  # Restore original scientific notation
+
+  colnames(df) <- c("parameters", seq(1, n_files))
+  #knitr::kable(t(file$parameters))
+  t <- knitr::kable(df)
+  options(scipen = old_scipen)
+  t
+}
