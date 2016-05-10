bc1bde7893011d1bc785a9e49f0df74b162d2930
FEMKE THON <femke_thon@hotmail.com>
2016-05-10 17:01:32 +0200

    added branch_sorter

--- R/normalized_rooted_branch_score.R
+++ R/normalized_rooted_branch_score.R
@@ -5,29 +5,18 @@
 #' @author Femke Thon
 
 # At some point, I want to start with reading a file - but for now, let's act
-# like x and y are vectors containing the lengths of the branches, in the right
+# like x and y are vectors containing the lengths of the branches, in the same
 # order. If a branch doesn't exist, it's represented by NA.
 
-normalized_rooted_branch_score <- function(x, y) {
-# starting with a few error messages
-  if (length(x) != length(y)){
-    stop("Vectors should be of equal length.")
-  }
-# filling up the positions of branches that aren't there. It has to be this
-# order, because otherwise, the next error is going to complain about NA not
-# being a number.
-  x <- replace(x, is.na(x), 0)
-  y <- replace(y, is.na(y), 0)
-
-  if (any(x > 1) || any(y > 1)){
-    stop("Branch lengths should be 1 or smaller.")
-  }
+normalized_rooted_branch_score <- function(tree1, tree2) {
+# preparing the trees
+ branch_sorter(tree1, tree2)
 
 # actually calculating the score
   score      <- 0
   tick       <- 1
-  for (i in x){
-    subscore <- ( (i - y[tick]) ^ 2)
+  for (branch in tree1){
+    subscore <- ( (branch - tree2[tick]) ^ 2)
     score    <- (score + subscore)
     tick     <- (tick + 1)
   }
