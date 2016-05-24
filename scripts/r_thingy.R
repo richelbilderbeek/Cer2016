d10d3b3be6ce55d57eb5d3cbb1dc3577c344f8b9
richelbilderbeek <richel@richelbilderbeek.nl>
2016-05-24 12:16:51 +0200

    Works, but must clean up

--- scripts/r_thingy.R
+++ scripts/r_thingy.R
@@ -1,9 +1,57 @@
+analysis_femke <- function(
+  df_species_trees,
+  df_posterior
+  n
+) {
+  comparison  <- NULL
+  counter2    <- 0
+  for (stat in head(df_posterior$gamma_stat, n = n)) {
+    counter  <- 0
+    counter2 <- counter2 + 1
+    if (!is.na(stat)){
+      for (value in df_species_trees$gamma_stat){
+        counter <- counter + 1
+        testit::assert(counter >= 1)
+        testit::assert(counter <= length(df_species_trees$filenames))
+        testit::assert(counter2 >= 1)
+        testit::assert(counter2 <= length(df_posterior$filenames))
+        testit::assert(df_posterior$filenames[counter2] != df_species_trees$filenames[counter])
+        if ((df_posterior$filenames[counter2] ==
+             df_species_trees$filenames[counter]) && !is.na(value)){
+          comparison$filenames  <- c(comparison$filenames,
+                                     df_species_trees$filenames[counter])
+          comparison$treenumber <- c(comparison$treenumber,
+                                     df_species_trees$species_tree[counter])
+          comparison$gamma_pbd  <- c(comparison$gamma_pbd, value)
+          comparison$gamma_post <- c(comparison$gamma_post, stat)
+          comparison$diff       <- c(comparison$diff,
+                                     (value - stat))
+          break
+        }
+      }
+    }
+  }
+  comparison <- data.frame(comparison)
+  comparison
+}
+
+analysis_richel <- function(
+  df_species_trees,
+  df_posterior
+) {
+  # Do an inner join
+  total <- merge(df_species_trees,df_posterior, by = c("filenames", "species_tree"))
+  total$diff <- total$gamma_stat.x - total$gamma_stat.y
+  total
+}
+
 csv_filename_species_trees <- "vignettes/collected_gammas_species_trees.csv"
 csv_filename_posterior     <- "vignettes/collected_gammas_posterior.csv"
 csv_filename_parameters    <- "vignettes/collected_parameters.csv"
 testit::assert(file.exists(csv_filename_species_trees))
 testit::assert(file.exists(csv_filename_posterior))
 testit::assert(file.exists(csv_filename_parameters))
+
 
 df_species_trees <- read.csv(
   file = csv_filename_species_trees,
@@ -26,42 +74,22 @@ df_parameters <- read.csv(
   row.names = 1
 )
 
-start_time <- Sys.time()
+names(df_species_trees)
+head(total)
 
-comparison  <- NULL
-counter2    <- 0
-for (stat in head(df_posterior$gamma_stat)) {
-  counter  <- 0
-  counter2 <- counter2 + 1
-  if (!is.na(stat)){
-    for (value in df_species_trees$gamma_stat){
-      counter <- counter + 1
-      testit::assert(counter >= 1)
-      testit::assert(counter <= length(df_species_trees$filenames))
-      testit::assert(counter2 >= 1)
-      testit::assert(counter2 <= length(df_posterior$filenames))
-      # testit::assert(df_posterior$filenames[counter2] != df_species_trees$filenames[counter])
-      if ((df_posterior$filenames[counter2] ==
-           df_species_trees$filenames[counter]) && !is.na(value)){
-        comparison$filenames  <- c(comparison$filenames,
-                                   df_species_trees$filenames[counter])
-        comparison$treenumber <- c(comparison$treenumber,
-                                   df_species_trees$species_tree[counter])
-        comparison$gamma_pbd  <- c(comparison$gamma_pbd, value)
-        comparison$gamma_post <- c(comparison$gamma_post, stat)
-        comparison$diff       <- c(comparison$diff,
-                                   (value - stat))
-        break
-      }
-    }
-  }
-}
-comparison <- data.frame(comparison)
-comparison
+profile_filename <- "rprof.out"
 
-end_time <- Sys.time()
-time_taken <- end_time - start_time
-print(time_taken)
+Rprof(filename = profile_filename)
+df <- analysis_femke(
+  df_species_trees = df_species_trees,
+  df_posterior = df_posterior,
+  df_parameters = df_parameters,
+  n = 2
+)
+print(nrow(df))
+Rprof(NULL)
+summaryRprof(file = profile_filename)
+
 
 knitr::kable(comparison)
 
@@ -70,6 +98,11 @@ df_time <- data.frame(
   t = c(1.69, 9.553, 24.96, 42.41, 120.2)
 )
 ggplot2::qplot(data = df_time, x = df_time$n, y = df_time$t)
+
+comparison <- analysis_richel(
+  df_species_trees = df_species_trees,
+  df_posterior = df_posterior
+)
 
 write.csv(
   x = comparison,
