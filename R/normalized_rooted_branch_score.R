165ba0566dd219bbc9d35069e776cef8fb52b512
richelbilderbeek <richel@richelbilderbeek.nl>
2016-05-27 16:47:10 +0200

    For @lintr-bot

--- R/normalized_rooted_branch_score.R
+++ R/normalized_rooted_branch_score.R
@@ -10,17 +10,17 @@
 
 normalized_rooted_branch_score <- function(filenames) {
 # # sorting the trees.
-#   for (filename in filenames){
-#     filename                <- Cer2016::branch_sorter(filename)
-#   }
+#   for (filename in filenames){ # nolint
+#     filename                <- Cer2016::branch_sorter(filename) # nolint
+#   } # nolint
 # # actually calculating the score
-#   score      <- 0
-#   tick       <- 1
+#   score      <- 0 # nolint
+#   tick       <- 1 # nolint
 #   PBD_output <- NULL # nolint variables cannot be named 'PBD-output', so I renamed it to 'PBD-output'
-#   for (posterior in groups){
-#     subscore <- ( (posterior - PBD_output[tick]) ^ 2)
-#     score    <- (score + subscore)
-#     tick     <- (tick + 1)
-#   }
+#   for (posterior in groups){ # nolint
+#     subscore <- ( (posterior - PBD_output[tick]) ^ 2) # nolint
+#     score    <- (score + subscore) # nolint
+#     tick     <- (tick + 1) # nolint
+#   } # nolint
  filenames
 }
