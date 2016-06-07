882e2191b0460ab49c67011dee43e67b50c073e1
richelbilderbeek <richel@richelbilderbeek.nl>
2016-06-07 14:14:58 +0200

    run_2 uses correctly written script file

--- scripts/run_2.sh
+++ scripts/run_2.sh
@@ -2,5 +2,5 @@
 # Call this file from this project root by './scripts/run_2.sh'
 for filename in `ls *.RDa`
 do
-  sbatch ./add_pbd_output.sh $filename
+  sbatch ./scripts/add_pbd_output.sh $filename
 done
