ded840051f8becec30816110de4a719686eeec44
Richel Bilderbeek <richelbilderbeek@users.noreply.github.com>
2016-06-09 10:08:19 +0200

    Meeting with Rampal

--- R/create_parameter_files_article.R
+++ R/create_parameter_files_article.R
@@ -5,15 +5,16 @@
 create_parameter_files_article <- function() {
   filenames <- NULL
   b_index <- 0
-  for (b in c(0.1, 0.5, 1.0)) {
+  for (b in c(0.3, 0.5)) {
     lambda_index <- 0
     for (lambda in c(0.1, 0.3, 1.0, 1000000)) {
       mu_index <- 0
       for (mu in c(0.0, 0.1, 0.2, 0.4)) {
+        if (mu > b) next
         r_index <- 0
         for (r in c(0.1, 0.01, 0.001)) {
           l_index <- 0
-          for (l in c(1000, 10000, 100000)) {
+          for (l in c(1000, 10000)) {
             filename <- paste0(
               "article_",
               b_index, "_",
