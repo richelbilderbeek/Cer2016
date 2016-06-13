4b77d3d7fa6080b0886ea7118cbbc9f4123eb1a7
richelbilderbeek <richel@richelbilderbeek.nl>
2016-06-13 14:53:03 +0200

    For @lintr-bot

--- tests/testthat/test-collect_species_tree_n_taxa.R
+++ tests/testthat/test-collect_species_tree_n_taxa.R
@@ -33,22 +33,18 @@ test_that("collect_species_tree_n_taxa: abuse", {
 
 test_that("collect_species_tree_n_taxa: silent", {
 
-  # NA file
-  expect_silent(
-    collect_species_tree_n_taxa(
-      filename = "/home/p230198/Peregrine/article_0_0_0_0_0.RDa",
-      verbose = TRUE
+  filename <- "/home/p230198/Peregrine/article_0_0_0_0_0.RDa"
+  if (file.exists(filename)) {
+    expect_silent(
+      collect_species_tree_n_taxa(filename = filename, verbose = TRUE
+      )
     )
-  )
+  }
 
-  # NA file
-  expect_silent(
-    collect_species_tree_n_taxa(
-      filename = "/home/p230198/Peregrine/article_1_3_0_0_0.RDa",
-      verbose = FALSE
+  filename <- "/home/p230198/Peregrine/article_1_3_0_0_0.RDa"
+  if (file.exists(filename)) {
+    expect_silent(
+      collect_species_tree_n_taxa(filename = filename)
     )
-  )
-
+  }
 })
-
-
