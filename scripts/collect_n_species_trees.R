793cc3e85a08b372bacfd3f2b0f0c3e24c7efa41
Richel Bilderbeek <richel@richelbilderbeek.nl>
2016-06-15 13:06:23 +0200

    Call correct function

--- scripts/collect_n_species_trees.R
+++ scripts/collect_n_species_trees.R
@@ -3,7 +3,7 @@ folder <- "/home/p230198/GitHubs/Cer2016/scripts"
 fns <- paste(
   folder, list.files(folder, pattern = "\\.RDa"), sep = "/"
 )
-df <- Cer2016::collect_n_species_trees(fns, verbose = TRUE)
+df <- Cer2016::collect_files_n_species_trees(fns, verbose = TRUE)
 write.csv(
   x = df,
   file = "collected_n_species_trees.csv",
