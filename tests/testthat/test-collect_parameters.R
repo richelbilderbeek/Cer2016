53d49c89beb85d2959a8604aec0240e2ea67775a
richelbilderbeek <richel@richelbilderbeek.nl>
2016-06-27 16:30:45 +0200

    Increase codecov

--- tests/testthat/test-collect_parameters.R
+++ tests/testthat/test-collect_parameters.R
@@ -17,4 +17,24 @@ test_that("collect_parameters: abuse", {
     collect_parameters(filenames = c(), verbose = "TRUE nor FALSE"),
     "collect_parameters: verbose should be TRUE or FALSE"
   )
+
+  # Create a 'corrupt file'
+  filename <- "test-collect_parameters.RDa"
+  cat("I am not a valid file\n", file = filename)
+
+  df <- collect_parameters(
+    filenames = c(
+      filename,
+      find_path("toy_example_1.RDa")
+    )
+  )
+  # One row per file
+  expect_equal(nrow(df), 2)
+
+  # File one has only NAs
+  expect_true(is.na(df[1, "rng_seed"]))
+
+  # File two (a toy example) is valid
+  expect_false(is.na(df[2, "rng_seed"]))
+  file.remove(filename)
 })
