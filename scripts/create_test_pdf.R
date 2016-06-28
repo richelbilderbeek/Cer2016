aa704cc87cc44e95e137342af4f62cf666fcad66
richelbilderbeek <richel@richelbilderbeek.nl>
2016-06-28 11:34:03 +0200

    Also use full path

--- scripts/create_test_pdf.R
+++ scripts/create_test_pdf.R
@@ -9,10 +9,14 @@ Sys.setenv(RSTUDIO_PANDOC="/usr/bin/pandoc")
 print(Sys.which("pandoc"))
 print(system("pandoc -v"))
 
+# Knitr
+
 tryCatch(
   knitr::knit("../vignettes/troubleshooting.Rmd", "troubleshooting_knitr.pdf"),
   error = function(msg) { message(msg) }
 )
+
+# Pandoc with squiggle
 
 tryCatch(
   rmarkdown::render("../vignettes/troubleshooting.Rmd", output_file = "~/troubleshooting_pandoc.html"),
@@ -21,5 +25,17 @@ tryCatch(
 
 tryCatch(
   system("pandoc ~/troubleshooting_pandoc.html -o troubleshooting_pandoc.pdf"),
+  error = function(msg) { message(msg) }
+)
+
+# Pandoc with full path
+
+tryCatch(
+  rmarkdown::render("../vignettes/troubleshooting.Rmd", output_file = "/home/p230198/troubleshooting_pandoc.html"),
+  error = function(msg) { message(msg) }
+)
+
+tryCatch(
+  system("pandoc /home/p230198/troubleshooting_pandoc.html -o troubleshooting_pandoc.pdf"),
   error = function(msg) { message(msg) }
 )
