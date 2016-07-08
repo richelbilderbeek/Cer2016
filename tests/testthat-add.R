10ffe818d4a1a86a74e5ae05de45b17ee0f5b8e3
richelbilderbeek <richel@richelbilderbeek.nl>
2016-07-08 10:01:11 +0200

    Do filtered tests in seperate files

---
+++ tests/testthat-add.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(Cer2016)
+
+test_check("Cer2016", filter = "add_")
---
+++ tests/testthat-has.R
@@ -0,0 +1,3 @@
+library(testthat)
+# library(Cer2016) # Really needed?
+test_check("Cer2016", filter = "has_")
