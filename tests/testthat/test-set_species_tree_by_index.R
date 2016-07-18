dc7cebd1aabae1311a870d16cc52bea27bc41a69
richelbilderbeek <richel@richelbilderbeek.nl>
2016-07-18 15:26:28 +0200

    Fixed script, increased codecov

---
+++ tests/testthat/test-set_species_tree_by_index.R
@@ -0,0 +1,39 @@
+context("set_species_tree_by_index")
+
+test_that("set_species_tree_by_index: use", {
+  file <- read_file(find_path("toy_example_1.RDa"))
+  st <- get_species_tree_by_index(file = file, sti = 1)
+  expect_true(is_phylogeny(st))
+  st_new <- ape::rcoal(10)
+  expect_false(ape::all.equal.phylo(st, st_new))
+  file <- set_species_tree_by_index(
+    file = file,
+    sti = 1,
+    species_tree = st_new
+  )
+  st_new_again <- get_species_tree_by_index(file = file, sti = 1)
+  expect_true(ape::all.equal.phylo(st_new, st_new_again))
+})
+
+
+test_that("set_species_tree_by_index: abuse", {
+
+  expect_error(
+    set_species_tree_by_index(
+      file = read_file(find_path("toy_example_1.RDa")),
+      sti = 0,
+      species_tree = ape::rcoal(10)
+    ),
+    "set_species_tree_by_index: index must be at least 1"
+  )
+
+  expect_error(
+    set_species_tree_by_index(
+      file = read_file(find_path("toy_example_1.RDa")),
+      sti = 3,
+      species_tree = ape::rcoal(10)
+    ),
+    "set_species_tree_by_index: index must be less than number of species_trees"
+  )
+
+})
