b771c040f88681b62f5993051bfddee358b0faa8
richelbilderbeek <richel@richelbilderbeek.nl>
2016-07-21 10:49:54 +0200

    Improved #95

--- scripts/collect_n_taxa.sh
+++ scripts/collect_n_taxa.sh
@@ -3,7 +3,7 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --ntasks=1
-#SBATCH --mem=1G
+#SBATCH --mem=10G
 #SBATCH --job-name=collect_n_taxa
 #SBATCH --output=collect_n_taxa.log
 module load R/3.3.1-foss-2016a
