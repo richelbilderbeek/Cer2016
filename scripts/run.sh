7bbdd67e4a86e196215234333846f3097ee18646
richelbilderbeek <richel@richelbilderbeek.nl>
2016-07-28 10:05:54 +0200

    Fixed #106

--- scripts/run.sh
+++ scripts/run.sh
@@ -9,13 +9,38 @@ rm *.RDa
 rm *.csv
 rm *.pdf
 rm *.md
+
+##########################
+# Update other packages
+##########################
+
+jobid=`sbatch install_r_packages.sh | cut -d ' ' -f 4`
+echo "jobid: "$jobid
+
+
+##########################
+# Update this package
+##########################
+
+cmd="sbatch --dependency=afterok:$jobid install_this_r_package.sh"
+echo "cmd: "$cmd
+jobid=`$cmd | cut -d ' ' -f 4`
+echo "jobid: "$jobid
+
 ##########################
 # Create parameter files
 ##########################
 
-#jobid=`sbatch create_test_parameter_files.sh | cut -d ' ' -f 4`
-jobid=`sbatch create_parameter_files_timings.sh | cut -d ' ' -f 4`
-#jobid=`sbatch create_parameter_files_article.sh | cut -d ' ' -f 4`
+
+# Trivial test runs
+# cmd="sbatch --dependency=afterok:$jobid create_test_parameter_files.sh"
+# 1% of MCMC run
+cmd="sbatch --dependency=afterok:$jobid create_parameter_files_timings.sh"
+# Full run
+#cmd="sbatch --dependency=afterok:$jobid create_parameter_files_article.sh"
+
+echo "cmd: "$cmd
+jobid=`$cmd | cut -d ' ' -f 4`
 echo "jobid: "$jobid
 
 ##########################
