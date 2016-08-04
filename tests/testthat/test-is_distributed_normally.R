e80e6fb2bf82cdda3c0327396e0131e345e6179f
richelbilderbeek <richel@richelbilderbeek.nl>
2016-08-04 09:35:22 +0200

    Removed obsolete test, fixed doc

--- tests/testthat/test-is_distributed_normally.R
+++ tests/testthat/test-is_distributed_normally.R
@@ -35,10 +35,4 @@ test_that("is_distributed_normally: abuse", {
     "is_distributed_normally: sample size must be between 3 and 5000"
   )
 
-  expect_error(
-    is_distributed_normally(rep(3.14, times = 10)),
-    "is_distributed_normally: all values are identical, with value "
-  )
-
-
 })
