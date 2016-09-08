97a6cc67de3515ca1fbeec249388d4c00b955717
richelbilderbeek <richel@richelbilderbeek.nl>
2016-09-08 09:32:59 +0200

    Fix test, add tidyr to Suggests as well

--- tests/testthat/test-are_beast_replicates_from_same_distribution.R
+++ tests/testthat/test-are_beast_replicates_from_same_distribution.R
@@ -1,18 +1,37 @@
 context("are_beast_replicates_from_same_distribution")
 
-test_that("dataset is too small", {
+test_that("dataset is too small sometimes", {
   # Collect the nLTT stats
   nltt_stats_raw <- read_collected_nltt_stats()
   # Remove the NAs
   nltt_stats <- nltt_stats_raw[!is.na(nltt_stats_raw$nltt_stat), ]
-  head(nltt_stats)
   df <- are_beast_replicates_from_same_distribution(nltt_stats)
-
   t <- dplyr::count(df, same_distr)
-  expect_true(is.na(t$same_distr[1]))
+
+  # There will be some NA's, but also some TRUEs and FALSEs
+  expect_true(is.na(t$same_distr[3]))
+  expect_true(t$n[3] > 1)
 })
 
-test_that("create artifical dataset", {
+test_that("create artifical dataset: same distributions", {
+
+  # Create a fake dataset with 100 nLTT statistics per BEAST2 posterior
+  # resulting in 200 rows in totla
+  nltt_stats <- data.frame(
+    filename = rep("fake.RDa", 200),
+    sti = rep(1, 200),
+    ai = rep(1, 200),
+    pi = rep(c(1,2), each = 100),
+    si = rep(1:100, 2),
+    nltt_stat = c(runif(100), runif(100))
+  )
+  df <- are_beast_replicates_from_same_distribution(nltt_stats)
+  t <- dplyr::count(df, same_distr)
+  expect_true(t$same_distr[1] == TRUE)
+  expect_true(t$n[1] == 1)
+})
+
+test_that("create artifical dataset: different distributions", {
 
   # Create a fake dataset with 100 nLTT statistics per BEAST2 posterior
   # resulting in 200 rows in totla
@@ -25,8 +44,7 @@ test_that("create artifical dataset", {
     nltt_stat = c(runif(100), rnorm(100))
   )
   df <- are_beast_replicates_from_same_distribution(nltt_stats)
-  df
-
   t <- dplyr::count(df, same_distr)
-  expect_true(is.na(t$same_distr[1]))
+  expect_true(t$same_distr[1] == FALSE)
+  expect_true(t$n[1] == 1)
 })
