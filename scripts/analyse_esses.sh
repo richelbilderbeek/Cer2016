32f03455b69f5c593d1a1b452ff3e78af506aae8
richelbilderbeek <richel@richelbilderbeek.nl>
2016-09-19 14:29:36 +0200

    Use proper .log filename

--- scripts/analyse_esses.sh
+++ scripts/analyse_esses.sh
@@ -4,7 +4,7 @@
 #SBATCH --ntasks-per-node=1
 #SBATCH --ntasks=1
 #SBATCH --mem=10G
-#SBATCH --job-name=collect_esses
-#SBATCH --output=collect_esses.log
+#SBATCH --job-name=analyse_esses
+#SBATCH --output=analyse_esses.log
 module load R/3.3.1-foss-2016a
 time Rscript analyse_esses.R
