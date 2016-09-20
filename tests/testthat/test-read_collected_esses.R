e37c145291e0572f8eb555d33f02b251b6a7d018
richelbilderbeek <richel@richelbilderbeek.nl>
2016-09-20 10:11:11 +0200

    Fix test

--- tests/testthat/test-read_collected_esses.R
+++ tests/testthat/test-read_collected_esses.R
@@ -3,7 +3,7 @@ context("read_collected_esses")
 test_that("read_collected_esses use", {
   df <- read_collected_esses()
   expected_names <- c("filename", "sti", "ai", "pi", "min_ess")
-  expect_equal(all(names(df) == expected_names))
+  expect_true(all(names(df) == expected_names))
   expect_true(is.factor(df$filename))
   expect_true(is.factor(df$sti))
   expect_true(is.factor(df$ai))
